78b54cea8c36a3c24a6fb1219d4832e4dd2ada08
Eugene Yokota <eed3si9n@gmail.com>
2020-01-24 12:30:29 -0500

    more linux release

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -4,7 +4,9 @@ set -eu
 releaseLinux() {
   git clone https://github.com/sbt/sbt-launcher-package.git
   pushd sbt-launcher-package
-  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false rpm:publish debian:publish
+  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false rpm:publish rpm:bintrayReleaseAllStaged debian:publish debian:bintrayReleaseAllStaged
+  curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/rpm
+  curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/debian
   popd
 }
 
