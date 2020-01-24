ec1450ea63ed4e76fb31ce7c97349800f4b82864
Eugene Yokota <eed3si9n@gmail.com>
2020-01-24 18:33:28 -0500

    GitHub Releases

--- bin/run-ci-win.sh
+++ bin/run-ci-win.sh
@@ -5,6 +5,7 @@ buildWindows() {
   git clone https://github.com/sbt/sbt-launcher-package.git
   pushd sbt-launcher-package
   sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=true windows:packageBin
+  mv target/windows/sbt.msi target/windows/sbt-$SBT_VER.msi
   popd
 }
 
