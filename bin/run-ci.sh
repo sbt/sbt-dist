46c3174dd166f652bf82fcafe948e7e5162417fa
Eugene Yokota <eed3si9n@gmail.com>
2020-11-04 23:14:36 -0500

    Don't include sbtn into Debian/RPM distribution
    Ref https://github.com/sbt/sbt/issues/6053

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -13,7 +13,7 @@ buildLinux() {
 releaseLinux() {
   git clone https://github.com/sbt/sbt-launcher-package.git
   pushd sbt-launcher-package
-  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false rpm:publish rpm:bintrayReleaseAllStaged debian:publish debian:bintrayReleaseAllStaged
+  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false -Dsbt.build.includesbtn=false  rpm:publish rpm:bintrayReleaseAllStaged debian:publish debian:bintrayReleaseAllStaged
   curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/rpm
   curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/debian
   popd
