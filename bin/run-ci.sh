93b45247fef6f43723542f5d2a422fac8ee325f0
Eugene Yokota <eed3si9n@gmail.com>
2021-04-24 18:10:51 -0400

    Linux release from sbt/sbt

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -2,8 +2,9 @@
 set -eu
 
 buildLinux() {
-  git clone https://github.com/sbt/sbt-launcher-package.git
-  pushd sbt-launcher-package
+  git clone https://github.com/sbt/sbt.git
+  pushd sbt
+  cd launcher-package
   sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false clean universal:packageBin universal:packageZipTarball
   mv target/universal/sbt.zip target/universal/sbt-$SBT_VER.zip
   mv target/universal/sbt.tgz target/universal/sbt-$SBT_VER.tgz
@@ -11,11 +12,18 @@ buildLinux() {
 }
 
 releaseLinux() {
-  git clone https://github.com/sbt/sbt-launcher-package.git
-  pushd sbt-launcher-package
-  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false -Dsbt.build.includesbtn=false  rpm:publish rpm:bintrayReleaseAllStaged debian:publish debian:bintrayReleaseAllStaged
-  curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/rpm
-  curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/debian
+  git clone https://github.com/sbt/sbt.git
+  pushd sbt
+  cd launcher-package
+  echo "credentials += Credentials(Path.userHome / \".sbt\" / \"credentials\")" > local.sbt
+  echo "realm = Artifactory Realm" >  $HOME/.sbt/credentials
+  echo "host = scala.jfrog.io"     >> $HOME/.sbt/credentials
+  echo "user = $BINTRAY_USER"      >> $HOME/.sbt/credentials
+  echo "password = $BINTRAY_PASS"  >> $HOME/.sbt/credentials
+  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false -Dsbt.build.includesbtn=false -Dsbt.build.includesbtlauncher=false rpm:publish debian:publish
+  rm -f $HOME/.sbt/credentials
+  # curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/rpm
+  # curl --user "$BINTRAY_USER:$BINTRAY_PASS" --data "" https://bintray.com/api/v1/calc_metadata/sbt/debian
   popd
 }
 
