72d0d7bf0e03a8096407f72fc2a9ba380fb1bddc
Eugene Yokota <eed3si9n@gmail.com>
2021-04-25 23:42:46 -0400

    Fix linux release
    using publish would make 32 byte artifact
    curl I think works ok

--- bin/run-ci.sh
+++ bin/run-ci.sh
@@ -21,10 +21,10 @@ releaseLinux() {
   echo "host = scala.jfrog.io"     >> $HOME/.sbt/credentials
   echo "user = $BINTRAY_USER"      >> $HOME/.sbt/credentials
   echo "password = $BINTRAY_PASS"  >> $HOME/.sbt/credentials
-  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false -Dsbt.build.includesbtn=false -Dsbt.build.includesbtlaunch=false rpm:publish debian:publish
+  sbt -Dsbt.build.version=$SBT_VER -Dsbt.build.offline=false -Dsbt.build.includesbtn=false -Dsbt.build.includesbtlaunch=false rpm:publish debian:packageBin
   rm -f $HOME/.sbt/credentials
 
-  # curl -H "X-JFrog-Art-Api:$BINTRAY_PASS" -XPUT "https://scala.jfrog.io/artifactory/debian/sbt-$SBT_VER.deb;deb.distribution=all;deb.component=main;deb.architecture=all" -T "target/sbt_$SBT_VER_all.deb"
+  curl -H "X-JFrog-Art-Api:$BINTRAY_PASS" -XPUT "https://scala.jfrog.io/artifactory/debian/sbt-$SBT_VER.deb;deb.distribution=all;deb.component=main;deb.architecture=all" -T "target/sbt_$SBT_VER_all.deb"
 
   # https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-CalculateDebianRepositoryMetadata
   curl --user "$BINTRAY_USER:$BINTRAY_PASS" https://scala.jfrog.io/artifactory/api/deb/reindex/debian --data ""
